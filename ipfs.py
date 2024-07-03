6f971512b6317ca7dac80a5f16fc16eac7b87e28
kumarh1776 <59850401+kumarh1776@users.noreply.github.com>
2024-07-02 22:33:53 -0400

    Add files via upload

--- ipfs.py
+++ ipfs.py
@@ -8,13 +8,13 @@ def pin_to_ipfs(data):
 	#YOUR CODE HERE
 		json_data = json.dumps(data)
 
-		response = reqeusts.post(f"{infura_url}/add", files = {"file": json_data})
+		response = requests.post(f"{infura_url}/add", files = {"file": json_data})
 
 		if response.status_code == 200:
 				cid = response.json()["Hash"]
 				return cid
 		else:
-				raise Expection(response.text)
+				raise Exception(response.text)
 
 def get_from_ipfs(cid,content_type="json"):
 		assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
@@ -27,6 +27,6 @@ def get_from_ipfs(cid,content_type="json"):
 						assert isinstance(data,dict), f"get_from_ipfs should return a dict"
 						return data
 				else:
-						raise Expection("Not dict")
+						raise Exception("Not dict")
 		else:
-				raise Expection(response.text)
+				raise Exception(response.text)
